877f70d96ea19d498aa9f612aca9c1b82b4c96d9
SaraAzmin <azmin.sara17@gmail.com>
2022-04-03 22:01:52 +0200

    reviews route connected to show all review button and all reviews loaded

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,7 +5,7 @@ const Review = ({ review }) => {
         <div className='border-2 border-indigo-800 p-5'>
             <div className='flex justify-between border-b-2 border-indigo-800 pb-3 mb-3'>
                 <h4 className='font-medium text-xl'>{review.name}</h4>
-                <h4 className='font-bold text-xl'>{review.rating}</h4>
+                <h4 className='font-bold text-2xl text-indigo-800'>{review.rating}</h4>
             </div>
             <p className='text-justify font-sans'>{review.review}</p>
         </div>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useReviews from '../../Hooks/useReviews';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useReviews([]);
+
     return (
-        <div>
-            <h1>reviews</h1>
+        <div className='mx-20'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 py-10'>
+                {
+
+                    reviews.map(review => <Review key={review.id} review={review}></Review>)
+                }
+            </div>
         </div>
     );
 };
