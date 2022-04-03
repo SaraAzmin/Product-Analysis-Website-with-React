61ae85421eebf76453af090cdb79a30b01fb57e6
SaraAzmin <azmin.sara17@gmail.com>
2022-04-03 19:15:56 +0200

    review data added and loaded with custom hook

---
+++ src/Hooks/useReviews.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+const useReviews = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviewData.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, []);
+
+    return [reviews, setReviews];
+
+}
+
+export default useReviews;
---
+++ src/components/CustomerReviewSection/CustomerReviewSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useReviews from '../../Hooks/useReviews';
+
+const CustomerReviewSection = () => {
+
+    const [reviews, setReviews] = useReviews([]);
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CustomerReviewSection;
